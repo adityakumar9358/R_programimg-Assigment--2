19299e089372546c44e2e31097b2efe1af785e0e
adityakumar9358 <127020407+adityakumar9358@users.noreply.github.com>
2023-03-30 10:27:15 +0530

    Create Catchmatrix.R

---
+++ Catchmatrix.R
@@ -0,0 +1,35 @@
+## Functions to cache inverse of matrix and retrieve its 
+##inverse from the cache
+
+## Creation of makeCache matrix object
+
+makeCacheMatrix <- function(m = matrix()) {
+    g <- NULL
+    set <- function(y) {
+            m <<- matrix
+            g <<- NULL
+    }
+    get <- function() m
+    setInverse <- function(inverse) g <<- inverse
+    getInverse <- function() g
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+}
+
+
+## Computing the inverse of the matric returned by makeCachematric
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+      ver <- x$getInverse()
+      if(!is.null(ver)) {
+          message("getting cached data")
+          return(ver)
+      }
+      data <- x$get()
+      ver <- solve(data)
+      x$setInverse(ver)
+      ver
+}
+Give feedback
